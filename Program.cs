135a336937cd640efb0e564c1ba4e6113f9a7893
狱中猫 <v154636v@163.com>
2026-05-22 20:55:52 +0800

    fix: CORS 增加 localhost:80 来源，修复 Docker 环境浏览器登录

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost", "http://localhost:80")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
